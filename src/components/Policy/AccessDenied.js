bb704ae385d21f693d8f3af522bc1254c2df5147
pinedabrafael <pinedabrafael@gmail.com>
2021-11-22 15:12:33 -0500

    Estilos mensaje de acceso negado

--- src/components/Policy/AccessDenied.js
+++ src/components/Policy/AccessDenied.js
@@ -4,8 +4,13 @@ import { Link } from "react-router-dom";
 const AccessDenied = ({ msn }) => {
   return (
     <Container>
-      <div className="jumbotron">
-        <h1 className="display-4">Acceso restringido!</h1>
+      <div className="jumbotron" style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <h1 className="display-5">Acceso restringido</h1>
         <p className="lead">{msn || "Contacta con el administrador"}</p>
         <Link to="/">
           <Button type="link">Regresar al inicio</Button>
